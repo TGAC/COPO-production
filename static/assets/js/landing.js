5eeab1209dea5694072846022139746c433ae4a8
Sherida101 <aaliyahsp@hotmail.com>
2024-02-29 16:33:09 +0000

    Removed unnecessary call to launch the 'Statistics' web page  when any element with the '.card' CSS class is clicked

--- static/assets/js/landing.js
+++ static/assets/js/landing.js
@@ -1,47 +1,45 @@
 $(document).ready(function () {
-    $(document).on("click", ".card", function () {
-        window.location = "/copo/tol_dashboard/stats#"
+  // $(document).on("click", ".card", function () {
+  //     window.location = "/copo/tol_dashboard/stats#"
+  // })
+
+  // $('.ui.dropdown').dropdown();
+  const image = getRandomInt(images.length);
+  $('body').css('background-image', 'url(' + images[image] + ')');
+
+  try {
+    var color = getRandomInt(content_classes.length);
+    $('#main_banner').addClass(content_classes[color]);
+  } catch (err) {}
+
+  $.getJSON('/api/stats/numbers')
+    .done(function (data) {
+      $('#num_samples').html(data.samples);
+      $('#num_profiles').html(data.profiles);
+      $('#num_users').html(data.users);
+      $('#num_uploads').html(data.datafiles);
     })
-
-    // $('.ui.dropdown').dropdown();
-    const image = getRandomInt(images.length);
-    $('body').css("background-image", "url(" + images[image] + ")")
-
-    try {
-        var color = getRandomInt(content_classes.length)
-        $("#main_banner").addClass(content_classes[color])
-    } catch (err) {
-
-    }
-
-
-    $.getJSON("/api/stats/numbers")
-        .done(function (data) {
-            $("#num_samples").html(data.samples)
-            $("#num_profiles").html(data.profiles)
-            $("#num_users").html(data.users)
-            $("#num_uploads").html(data.datafiles)
-        }).fail(function (data) {
-        console.log(data)
-    })
-    setActiveNavItem() // Set current web page as active nav bar item in top navbar
+    .fail(function (data) {
+      console.log(data);
+    });
+  setActiveNavItem(); // Set current web page as active nav bar item in top navbar
 });
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 function setActiveNavItem() {
-    const activePage = window.location.href;
+  const activePage = window.location.href;
 
-    $('.nav li a').filter(function () {
-        let linkPage = `${this.href}/`;
-        // home page i.e. COPO front page URL will end with '//' so it needs to be ommitted
-        linkPage = linkPage.endsWith('//') ? this.href : linkPage;
+  $('.nav li a').filter(function () {
+    let linkPage = `${this.href}/`;
+    // home page i.e. COPO front page URL will end with '//' so it needs to be ommitted
+    linkPage = linkPage.endsWith('//') ? this.href : linkPage;
 
-        if (activePage === linkPage) {
-            $(this).parent().addClass("active");
-            $(this).parent().append('<span class="sr-only">(current)</span>')
-        }
-    });
+    if (activePage === linkPage) {
+      $(this).parent().addClass('active');
+      $(this).parent().append('<span class="sr-only">(current)</span>');
+    }
+  });
 }
