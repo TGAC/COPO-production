0728bb93660f674f07566ebc90f9bc8e927ac413
debbyku <debbyku.2008@gmail.com>
2025-09-26 09:36:39 +0000

    update

--- src/main_config/settings/data.py
+++ src/main_config/settings/data.py
@@ -129,7 +129,7 @@ SINGLE_CELL_SCHEMAS_URL = {
     "COPO_SINGLE_CELL": f"https://github.com/TGAC/COPO-schemas/raw/refs/heads/main/single_cell/singlecell_schema_main_v{MANIFEST_VERSION['COPO_SINGLE_CELL']}.xlsx",
     "COPO_IMAGE_REMBI": f"https://github.com/TGAC/COPO-schemas/raw/refs/heads/main/images/rembi_image_schema_main_v{MANIFEST_VERSION['COPO_IMAGE_REMBI']}.xlsx",
     "COPO_IMAGE_STX_FISH": f"https://github.com/TGAC/COPO-schemas/raw/refs/heads/main/images/stx_fish_image_schema_main_v{MANIFEST_VERSION['COPO_IMAGE_STX_FISH']}.xlsx",
-    "COPO_READ": f"reads_schema_main_v{MANIFEST_VERSION['COPO_READ']}.xlsx",
+    "COPO_READ": f"https://github.com/TGAC/COPO-schemas/raw/refs/heads/main/reads/reads_schema_main_v{MANIFEST_VERSION['COPO_READ']}.xlsx",
 }
 
 IMAGE_FILE_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.gif', '.tiff', '.bmp']
