fddd6b57fd8f3db691195095c1e2e2e103023b10
Sherida101 <aaliyahsp@hotmail.com>
2025-11-20 16:53:12 +0000

    temporary edit: Try to see 'Errors occurred' error is shown and page doesn't load

--- src/apps/copo_core/middlewares/LogUncaughtExceptions.py
+++ src/apps/copo_core/middlewares/LogUncaughtExceptions.py
@@ -4,24 +4,23 @@ from common.utils.logger import Logger
 from common.lookup.copo_enums import Loglvl
 from django.http import HttpResponse
 
-#import logging
+import logging
 
-#logging = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 class LogUncaughtExceptions:
 
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        #logging.info("Processing " + request.build_absolute_uri())
+        # logging.info("Processing " + request.build_absolute_uri())
         try:
             return self.get_response(request)
         except Exception as e:
             Logger().log(e, level=Loglvl.ERROR)
             Logger().log(traceback.format_exc(), level=Loglvl.ERROR)
-            #raise e
-            return HttpResponse(content="Errors occurred", status=503)
-        #finally:
-            #logging.info("Processed " + request.build_absolute_uri())
-
-
+            logger.error("Uncaught exception in request", exc_info=True)
+            raise #raise e
+            # return HttpResponse(content="Errors occurred", status=503)
+        # finally:
+        # logging.info("Processed " + request.build_absolute_uri())
